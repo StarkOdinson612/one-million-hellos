025a44caf9e772cbc99469f1d659c8649000b0a4
StarkOdinson612 <darkknightishaan@gmail.com>
2021-05-14 11:44:40 +0530

    revert

--- static/javascript/refreshAPI.js
+++ static/javascript/refreshAPI.js
@@ -1,12 +1,12 @@
-// setInterval(async function() {
-//     await fetch("https://one-million-hellos.herokuapp.com/api")
-//     .then(response => response.json())
-//     .then(data => {
-//     //here you need to do the action to change the data on the page
-//         document.getElementById("list").innerHTML = data['text'];
-//         console.log("I HAVE ATTEMPTED TO REFRESH THE PAGE!");
-//     })
-//     .catch((error) => {
-//         console.error('Error:', error);
-//     });
-// }, 300);
+setInterval(async function() {
+    await fetch("https://one-million-hellos.herokuapp.com/api")
+    .then(response => response.json())
+    .then(data => {
+    //here you need to do the action to change the data on the page
+        document.getElementById("list").innerHTML = data['text'];
+        console.log("I HAVE ATTEMPTED TO REFRESH THE PAGE!");
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}, 300);
